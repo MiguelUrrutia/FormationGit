6dd9539a617332f0af71dfc63d43a7ebcccdf83b
MiguelUrrutia <miguel.urrutia@ecofog.gf>
2018-05-04 14:45:44 -0300

    Multiple

---
+++ Creating_a_pack.R
@@ -0,0 +1,13 @@
+Package: multiple
+Title: Compute multiples
+Version: 0.0-0.9000
+Authors@R: person("Miguel", "Urrutia", email = 
+                    "miguel.urrutia@ecofog.gf", role = c("aut", "cre"))
+Description: Efficiently calculate multiples of numbers, 
+following Me et al. (2018).
+Depends: R (>= 3.4.3)
+License: GNU General Public License
+Encoding: UTF-8
+LazyData: true
+
+#Creating a package (first trial)
